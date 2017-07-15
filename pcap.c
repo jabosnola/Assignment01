afb0c813d478659204955ab21ccca31d0e0634b9
jabosnola <jabosnola@gmail.com>
2017-07-15 02:21:22 -0700

    upload pcap.c - all thingprepared but need to compare with wireshark!

--- pcap.c
+++ pcap.c
@@ -12,6 +12,9 @@ void pcap(pcap_t *handle, struct pcap_pkthdr *header, const u_char *packet)
 	int status;
 	struct ether_header *ether;
 	char buf[1000];
+	struct ip *ipv4;
+	struct tcphdr *tcp;
+	int data_loc;
 
 	while(1)
 	{
@@ -25,16 +28,36 @@ void pcap(pcap_t *handle, struct pcap_pkthdr *header, const u_char *packet)
 		ether = (struct ether_header*)packet;
 
 		//Use ether_ntoa_r for reentrant thread-safe//
-		printf("S o u r c e  MAC : %s\n", ether_ntoa_r(ether->ether_shost, buf));
-		printf("Destionation MAC : %s\n", ether_ntoa_r(ether->ether_dhost, buf));
+		printf("S o u r c e MAC : %s\n", ether_ntoa_r(ether->ether_shost, buf));
+		printf("Destination MAC : %s\n", ether_ntoa_r(ether->ether_dhost, buf));
 
 		//printf("ether type : %x\n", ether->ether_type, buf);
 		
-		if(ntohs(ether->ether_type) != ETHERTYPE_IP)
+		if(ntohs(ether->ether_type) != ETHERTYPE_IP) // To change system order
 			//printf("IPv4\n");
 			continue;
+		//printf("%d\n", ETH_HLEN);
+		ipv4 = (struct ip*)(packet + ETH_HLEN);
 
-		
+		printf("S o u r c e IP : %s\n", inet_ntoa(ipv4->ip_src));
+		printf("Destination IP : %s\n", inet_ntoa(ipv4->ip_dst));
+
+		if(ipv4->ip_p != IPPROTO_TCP)
+			continue;
+
+		tcp	= (struct tcphdr*)(packet + ETH_HLEN + 4 * ipv4->ip_hl);
+
+		printf("S o u r c e Port : %d\n", tcp->th_sport);
+		printf("Destination Port : %d\n", tcp->th_dport);
+
+		data_loc = ETH_HLEN + 4 * ipv4->ip_hl + 4 * tcp->th_off; // Data located in after Ethernet header, IP header, TCP header(TCP offset)
+
+		printf("< Data Part >\n");
+
+		for(int i = data_loc; i<data_loc+10; i++)
+		{
+			printf("%02x\n", packet[i]);
+		}
 		
 	}
 	
